2079d14335529a4df96b4e286dad85a85a52438a
Eleonore Stultjens <eleonore.jens@gmail.com>
2022-10-21 16:46:37 +0200

    panier update onload

--- panierAjouter.php
+++ panierAjouter.php
@@ -1,28 +1,33 @@
 <?php
 session_start();
 
-$idFilm = $_POST['idFilm'];
-$prix = $_POST['prixFilm'];
-$titre = $_POST['titreFilm'];
-$quantite = (int) $_POST['quantite'];
-if ($quantite < 0){
-    $quantite = -($quantite);
-}
+$res = [];
 
-// si le panier n'existe pas déjà dans la session on le crée
-if(!isset($_SESSION['panier'])){
-    $_SESSION['panier'] = [];
-}
+if (!empty($_POST)) {
+    $idFilm = $_POST['idFilm'];
+    $prix = $_POST['prixFilm'];
+    $titre = $_POST['titreFilm'];
+    $quantite = (int) $_POST['quantite'];
+    if ($quantite < 0) {
+        $quantite = - ($quantite);
+    }
 
-if (!isset($_SESSION['panier'][$idFilm])){
-    //le produit n'existe pas on le remplit
-    $_SESSION['panier'][$idFilm] = ['nom' => $titre,
-                                    'quantite' => $quantite,
-                                    'prixUnitaire' =>$prix];
-}
-else {
-    // incrementer la quantite
-    $_SESSION['panier'][$idFilm]['quantite'] += $quantite;
-}
+    // si le panier n'existe pas déjà dans la session on le crée
+    if (!isset($_SESSION['panier'])) {
+        $_SESSION['panier'] = [];
+    }
 
-echo json_encode($_SESSION['panier']);
+    if (!isset($_SESSION['panier'][$idFilm])) {
+        //le produit n'existe pas on le remplit
+        $_SESSION['panier'][$idFilm] = [
+            'nom' => $titre,
+            'quantite' => $quantite,
+            'prixUnitaire' => $prix
+        ];
+    } else {
+        // incrementer la quantite
+        $_SESSION['panier'][$idFilm]['quantite'] += $quantite;
+    }
+}
+$res = $_SESSION['panier'];
+echo json_encode($res);
--- script/panier.js
+++ script/panier.js
@@ -1,51 +1,7 @@
 document.querySelectorAll(".ajouter").forEach((elem) => {
     elem.addEventListener('click', (e) => {
-        let quantite = e.target.previousSibling.value; // la valeur de l'input qui est à côté du button target // la value est en string 
-
-        let idFilm = e.target.dataset.id;
-        let prixFilm = e.target.dataset.prix;
-        let titreFilm = e.target.dataset.titre;
-
-        let xhr = new XMLHttpRequest();
-        let formulaire = new FormData(); // pour remplacer les balises form
-        formulaire.append("idFilm", idFilm);
-        formulaire.append("titreFilm", titreFilm);
-        formulaire.append("prixFilm", prixFilm);
-        formulaire.append("quantite", quantite);
-
-        xhr.onreadystatechange = (event) => {
-            if (xhr.readyState == 4) {
-                if (xhr.status == 200) {
-                    // console.log(xhr.responseText)
-                    let panier = document.getElementById('panier');
-                    panier.innerHTML = "";
-                    let reponse = JSON.parse(xhr.responseText);
-                    // console.log(reponse);
-
-                    // parcourir le panier qui est sous forme d'objet d'objets
-                    for (const idFilm in reponse) {
-                        let p = document.createElement('p');
-                        p.innerHTML = "";
-
-                        console.log(reponse[idFilm]);
-                        // p.innerHTML += ` ${reponse[idFilm][i]}`;
-                        p.innerHTML = `Produit: ${reponse[idFilm].nom} ----`;
-                        p.innerHTML += ` Quantité: ${reponse[idFilm].quantite} ----`;
-                        p.innerHTML += ` Prix: ${reponse[idFilm].quantite * reponse[idFilm].prixUnitaire} €`;
-
-                        panier.appendChild(p);
-                    }
-
-                    // reponse = Object.entries(reponse);
-                    // // rafraichir le panier dans la div
-                    // console.log(reponse);
-                }
-                else console.log("error: " + xhr.status);
-            }
-        }
-        xhr.open("POST", "./panierAjouter.php");
-        xhr.send(formulaire);
-    })
+        updatePanier(e);
+    });
 });
 
 document.getElementById('vide').addEventListener('click', (e) => {
@@ -67,3 +23,59 @@ document.getElementById('vide').addEventListener('click', (e) => {
     xhr.open("GET", "./panierVider.php");
     xhr.send(null);
 });
+
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    updatePanier();
+});
+
+function updatePanier(e) {
+
+
+    let xhr = new XMLHttpRequest();
+    let formulaire = new FormData(); // pour remplacer les balises form
+
+    if (e != undefined) {    
+        let quantite = e.target.previousSibling.value; // la valeur de l'input qui est à côté du button target // la value est en string 
+        let idFilm = e.target.dataset.id;
+        let prixFilm = e.target.dataset.prix;
+        let titreFilm = e.target.dataset.titre;
+        formulaire.append("idFilm", idFilm);
+        formulaire.append("titreFilm", titreFilm);
+        formulaire.append("prixFilm", prixFilm);
+        formulaire.append("quantite", quantite);
+    }
+
+    xhr.onreadystatechange = (event) => {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                // console.log(xhr.responseText)
+                let panier = document.getElementById('panier');
+                panier.innerHTML = "";
+                let reponse = JSON.parse(xhr.responseText);
+                // console.log(reponse);
+
+                // parcourir le panier qui est sous forme d'objet d'objets
+                for (const idFilm in reponse) {
+                    let p = document.createElement('p');
+                    p.innerHTML = "";
+
+                    console.log(reponse[idFilm]);
+                    // p.innerHTML += ` ${reponse[idFilm][i]}`;
+                    p.innerHTML = `Produit: ${reponse[idFilm].nom} ----`;
+                    p.innerHTML += ` Quantité: ${reponse[idFilm].quantite} ----`;
+                    p.innerHTML += ` Prix: ${reponse[idFilm].quantite * reponse[idFilm].prixUnitaire} €`;
+
+                    panier.appendChild(p);
+                }
+
+                // reponse = Object.entries(reponse);
+                // // rafraichir le panier dans la div
+                // console.log(reponse);
+            }
+            else console.log("error: " + xhr.status);
+        }
+    }
+    xhr.open("POST", "./panierAjouter.php");
+    xhr.send(formulaire);
+}
